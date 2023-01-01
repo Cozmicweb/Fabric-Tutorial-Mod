8ee303ab6a758b91ba5d149cf054375bb3933d9b
Cozmicweb <jacknoetzel@gmail.com>
2023-01-01 11:39:34 -0500

    Removed comment

--- src/main/java/net/cozmic/tutorialmod/TutorialMod.java
+++ src/main/java/net/cozmic/tutorialmod/TutorialMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// fortnite
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
